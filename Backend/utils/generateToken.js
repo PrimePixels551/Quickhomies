2e0c1fb5f8be5bd4f8ba06a2aeb24abf0ea87846
Mohit776 <mohitaggarwal551@gmail.com>
2026-01-13 00:35:56 +0530

    jwt

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET || 'secret123', {
-        expiresIn: '30d',
+        expiresIn: '1y',
     });
 };
 
